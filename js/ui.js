edd3a73133d13b081393eadcd0b2fc6a13cd438c
Peter Jozsef <j.peter@camelweb.com>
2019-12-19 11:03:20 +0200

    PWA #3

---
+++ js/ui.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".btn").click(function(){
+        start = $("#startbpm").val();
+        end = $("#endbpm").val();
+
+        diff = (end*100/start)-100;
+        $(".transfer").html(diff.toFixed(2)+"%");
+    });
+});
